650d5e885e70d212d95ee44a79f22fb1032c7493
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2026-05-06 20:24:22 +0600

    Updates: updates

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"compiler/ast"
 	"compiler/lexer"
 	"testing"
 )
@@ -18,4 +19,42 @@ func TestLetStatement(t *testing.T) {
 	if len(program.Statements) != 3 {
 		t.Fatalf("program.Statements does not contain 3 statements, got=%d", len(program.Statements))
 	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let', got=%q", s.TokenLiteral())
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+
+	if !ok {
+		t.Errorf("s not *ast.LetStatement, got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s', got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s', got=%s", name, letStmt.Name.TokenLiteral)
+		return true
+	}
+	return true
 }
